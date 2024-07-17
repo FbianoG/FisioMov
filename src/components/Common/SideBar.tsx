5284ac500521cd0709938e3b51a2826b5632fb5c
Fabiano Gonçalves <boriscasoy123@gmail.com>
2024-07-16 21:26:44 -0300

    update Modal.css, SideBar.css and SideBar.tsx

--- src/components/Common/SideBar.tsx
+++ src/components/Common/SideBar.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { UserData } from '../../interface/interface'
 import './SideBar.css'
 
@@ -13,41 +14,43 @@ const SideBar: React.FC<SideBarProps> = ({ user }) => {
         if (location.pathname === href) return 'active'
     }
 
+    const [show, setShow] = useState<boolean>(false)
+
+    const handleShow = () => setShow(true)
+    const handleClose = () => setShow(false)
 
 
     return (
-        <div className="sideBar">
-            <h1>FisioMov</h1>
-
-            {!user?.isPatient &&
-                <ul className="sideBar__list">
-                    <a href='/fisio' className={verifyLinkActive('/fisio')}>Pacientes</a >
-                    <a href='/activities' className={verifyLinkActive('/activities')}>Atividades</a>
-                    <a href='/dashboard' className={verifyLinkActive('/dashboard')}>Dashboard</a>
-                    <a href='' className={verifyLinkActive('/')}>Configurações</a>
-                    <a href='/'>Sair</a>
-                </ul>
-            }
-            {user?.isPatient &&
-                <ul className="sideBar__list">
-                    <a href='/patient' className={verifyLinkActive('/patient')}>Atividades</a>
-                    <a href='' className={verifyLinkActive('/config')}>Configurações</a>
-                    <a href='/'>Sair</a>
-                </ul>
-            }
-            <div className="userItem">
-                <img src={user?.src} alt='foto perfil' />
-                <div className="userItem__data">
-                    <p>{user?.name.split(' ').slice(0, 1)} {user?.name.split(' ').slice(1, 2).join('').slice(0, 1)}.</p>
-                    <span>{user?.email}</span>
+        <>
+            {show && <div className="backdrop" onClick={handleClose}></div>}
+            {innerWidth < 1100 && <button title='Mostrar barra lateral' className="sideBar__btn" onClick={handleShow}>☰</button>}
+            <div className="sideBar" style={show ? { transform: 'translate(0)' } : {}}>
+                <h1>FisioMov</h1>
+                {!user?.isPatient &&
+                    <ul className="sideBar__list">
+                        <a href='/fisio' className={verifyLinkActive('/fisio')}>Pacientes</a >
+                        <a href='/activities' className={verifyLinkActive('/activities')}>Atividades</a>
+                        <a href='/dashboard' className={verifyLinkActive('/dashboard')}>Dashboard</a>
+                        <a href='' className={verifyLinkActive('/')}>Configurações</a>
+                        <a href='/'>Sair</a>
+                    </ul>
+                }
+                {user?.isPatient &&
+                    <ul className="sideBar__list">
+                        <a href='/patient' className={verifyLinkActive('/patient')}>Atividades</a>
+                        <a href='' className={verifyLinkActive('/config')}>Configurações</a>
+                        <a href='/'>Sair</a>
+                    </ul>
+                }
+                <div className="userItem">
+                    <img src={user?.src} alt='foto perfil' />
+                    <div className="userItem__data">
+                        <p>{user?.name.split(' ').slice(0, 1)} {user?.name.split(' ').slice(1, 2).join('').slice(0, 1)}.</p>
+                        <span>{user?.email}</span>
+                    </div>
                 </div>
             </div>
-            {innerWidth < 1100 &&
-                <button title='Mostrar barra lateral' className="sideBar__btn">
-                    ☰
-                </button>
-            }
-        </div>
+        </>
     )
 }
 
